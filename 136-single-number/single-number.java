7be33bb7475db7eb06f19230fbcc4ceb3f6c9610
Abhishek Verma <abhi.verma.avv@gmail.com>
2025-08-14 22:43:17 +0530

    Time: 1 ms (99.84%) | Memory: 46.3 MB (46.48%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+//  Abhishek Verma
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for( int i = 0; i<nums.length; i++) {
+            result = result^nums[i];
+        }
+        return result;
+    }
+}
